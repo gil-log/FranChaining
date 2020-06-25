c9e988df408665488b6f1388ad131798359e6dcb
swgil007 <swgil009@gmail.com>
2020-06-25 17:24:56 +0900

    Version 0.0.3
    - 본사, 지점 첫 페이지 나누기

--- FranChaining/src/main/java/com/franchaining/controller/HomeController.java
+++ FranChaining/src/main/java/com/franchaining/controller/HomeController.java
@@ -1,13 +1,8 @@
 package com.franchaining.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,12 +14,16 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	@RequestMapping(value = "/franchaining", method = RequestMethod.GET)
 	public String home(){
-		logger.info("Welcome main!");
+		logger.info("franchaining");
+
+		return "franchain";
+	}
+	
+	@RequestMapping(value = "/franchaining/main", method = RequestMethod.GET)
+	public String main(){
+		logger.info("main");
 
 		return "main";
 	}
--- FranChaining/src/main/webapp/WEB-INF/views/main.jsp
+++ FranChaining/src/main/webapp/WEB-INF/views/main.jsp
@@ -1,6 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page language="java" pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" %>
-<html lang="en">
+<html>
 
 <head>
 
