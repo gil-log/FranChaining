9699bc7922cdbf69a5ebf68e8818a2db9fcb933b
sewoongpark <63997044+sewoongpark@users.noreply.github.com>
2020-07-03 17:58:06 +0900

    0.0.6-woong-1
    jsp파일 수정

--- FranChaining/src/main/java/com/franchaining/controller/BranchController.java
+++ FranChaining/src/main/java/com/franchaining/controller/BranchController.java
@@ -2,6 +2,7 @@ package com.franchaining.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import javax.inject.Inject;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.franchaining.service.BranchService;
 import com.franchaining.service.EmpService;
 import com.franchaining.service.ManagerService;
+import com.franchaining.vo.EmpVO;
 import com.franchaining.vo.ManagerVO;
 import com.franchaining.vo.RegVO;
 
@@ -38,5 +40,35 @@ public class BranchController {
 	BranchService branchService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(BranchController.class);
+	
+	@RequestMapping(value = "/manager/main", method = RequestMethod.GET)
+	public String manger(){
+		logger.info("/manager/main");
+
+		return "/branch/manager/manager_main";
+	}
+	
+	@RequestMapping(value = "/manager/stock", method = RequestMethod.GET)
+	public String managerStockget(Model model) throws Exception {
+		logger.info("mgrStkget");
+
+		return "branch/manager/manager_stock";
+	}
+	
+	@RequestMapping(value = "/manager/order", method = RequestMethod.GET)
+	public String managerOrderget(Model model) throws Exception {
+		logger.info("mgrOdrget");
+
+		return "branch/manager/manager_order";
+	}
+	
+	@RequestMapping(value = "/manager/orderlist", method = RequestMethod.GET)
+	public String managerOrderListget(Model model) throws Exception {
+		logger.info("mgrOLget");
+
+		return "branch/manager/manager_orderlist";
+	}
+	
+	
 	
 }
